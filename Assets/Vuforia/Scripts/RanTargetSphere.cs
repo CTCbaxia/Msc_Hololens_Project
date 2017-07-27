5f66c0a656c7c0bb91bee3cc19995bdd8a6820d8
CTCbaxia <ctcbaxia@gmail.com>
2017-07-27 23:41:07 +0100

    new

--- Assets/Vuforia/Scripts/RanTargetSphere.cs
+++ Assets/Vuforia/Scripts/RanTargetSphere.cs
@@ -4,65 +4,60 @@ using UnityEngine;
 
 public class RanTargetSphere : MonoBehaviour {
 
-	public GameObject sphere1;
-	public GameObject sphere2;
-	public GameObject sphere3;
-	public GameObject sphere4;
-	public GameObject sphere5;
-	public GameObject sphere6;
-	public GameObject sphere7;
-//	Random Ran = new Random();
+	public GameObject Dot1, Dot2, Dot3, Dot4, Dot5, Dot6, Dot7;
 
 	// Use this for initialization
 	void Start () {
-		sphere1.SetActive (false);
-		sphere2.SetActive (false);
-		sphere3.SetActive (false);
-		sphere4.SetActive (false);
-		sphere5.SetActive (false);
-		sphere6.SetActive (false);
-		sphere7.SetActive (false);
+		Dot1.SetActive (false);
+		Dot2.SetActive (false);
+		Dot3.SetActive (false);
+		Dot4.SetActive (false);
+		Dot5.SetActive (false);
+		Dot6.SetActive (false);
+		Dot7.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//test with keyboard to show corresponding Dot
 		if (Input.GetKeyDown(KeyCode.Q)){
 
-			SphPosition_1 ();
+			DotPosition_1 ();
 			print(" keyQ was pressed");
 			print (Random.Range (0,10));
 		}
 		if (Input.GetKeyDown(KeyCode.W)){
 
-			SphPosition_2 ();
+			DotPosition_2 ();
 			print(" keyW was pressed");
 
 		}
 		if (Input.GetKeyDown (KeyCode.E)) {
 
-			SphPosition_3 ();
+			DotPosition_3 ();
 			print(" keyE was pressed");
 		}
 		if (Input.GetKeyDown (KeyCode.R)) {
 
-			SphPosition_4 ();
+			DotPosition_4 ();
 			print(" keyR was pressed");
 
 		}
 		if (Input.GetKeyDown (KeyCode.T)) {
 
-			SphPosition_5 ();
+			DotPosition_5 ();
 			print(" keyT was pressed");
 
 		}
 		if (Input.GetKeyDown (KeyCode.Y)) {
 
-			SphPosition_6 ();
+			DotPosition_6 ();
 			print(" keyY was pressed");
 		}
 		if (Input.GetKeyDown (KeyCode.U)) {
 
-			SphPosition_7 ();
+			DotPosition_7 ();
 			print(" keyU was pressed");
 		}
 
@@ -70,76 +65,76 @@ public class RanTargetSphere : MonoBehaviour {
 	}
 
 
-
-	void SphPosition_1(){
-		sphere1.SetActive (true);
-		sphere2.SetActive (false);
-		sphere3.SetActive (false);
-		sphere4.SetActive (false);
-		sphere5.SetActive (false);
-		sphere6.SetActive (false);
-		sphere7.SetActive (false);
+	//show corresponding Dot 
+	void DotPosition_1(){
+		Dot1.SetActive (true);
+		Dot2.SetActive (false);
+		Dot3.SetActive (false);
+		Dot4.SetActive (false);
+		Dot5.SetActive (false);
+		Dot6.SetActive (false);
+		Dot7.SetActive (false);
 
 
 
 	}
 
-	void SphPosition_2(){
-		sphere1.SetActive (false);
-		sphere2.SetActive (true);
-		sphere3.SetActive (false);
-		sphere4.SetActive (false);
-		sphere5.SetActive (false);
-		sphere6.SetActive (false);
-		sphere7.SetActive (false);
+	void DotPosition_2(){
+		Dot1.SetActive (false);
+		Dot2.SetActive (true);
+		Dot3.SetActive (false);
+		Dot4.SetActive (false);
+		Dot5.SetActive (false);
+		Dot6.SetActive (false);
+		Dot7.SetActive (false);
 
 	
 	}
 
-	void SphPosition_3(){
-		sphere1.SetActive (false);
-		sphere2.SetActive (false);
-		sphere3.SetActive (true);
-		sphere4.SetActive (false);
-		sphere5.SetActive (false);
-		sphere6.SetActive (false);
-		sphere7.SetActive (false);
+	void DotPosition_3(){
+		Dot1.SetActive (false);
+		Dot2.SetActive (false);
+		Dot3.SetActive (true);
+		Dot4.SetActive (false);
+		Dot5.SetActive (false);
+		Dot6.SetActive (false);
+		Dot7.SetActive (false);
 	}
 
-	void SphPosition_4(){
-		sphere1.SetActive (false);
-		sphere2.SetActive (false);
-		sphere3.SetActive (false);
-		sphere4.SetActive (true);
-		sphere5.SetActive (false);
-		sphere6.SetActive (false);
-		sphere7.SetActive (false);
+	void DotPosition_4(){
+		Dot1.SetActive (false);
+		Dot2.SetActive (false);
+		Dot3.SetActive (false);
+		Dot4.SetActive (true);
+		Dot5.SetActive (false);
+		Dot6.SetActive (false);
+		Dot7.SetActive (false);
 	}
-	void SphPosition_5(){
-		sphere1.SetActive (false);
-		sphere2.SetActive (false);
-		sphere3.SetActive (false);
-		sphere4.SetActive (false);
-		sphere5.SetActive (true);
-		sphere6.SetActive (false);
-		sphere7.SetActive (false);
+	void DotPosition_5(){
+		Dot1.SetActive (false);
+		Dot2.SetActive (false);
+		Dot3.SetActive (false);
+		Dot4.SetActive (false);
+		Dot5.SetActive (true);
+		Dot6.SetActive (false);
+		Dot7.SetActive (false);
 	}
-	void SphPosition_6(){
-		sphere1.SetActive (false);
-		sphere2.SetActive (false);
-		sphere3.SetActive (false);
-		sphere4.SetActive (false);
-		sphere5.SetActive (false);
-		sphere6.SetActive (true);
-		sphere7.SetActive (false);
+	void DotPosition_6(){
+		Dot1.SetActive (false);
+		Dot2.SetActive (false);
+		Dot3.SetActive (false);
+		Dot4.SetActive (false);
+		Dot5.SetActive (false);
+		Dot6.SetActive (true);
+		Dot7.SetActive (false);
 	}
-	void SphPosition_7(){
-		sphere1.SetActive (false);
-		sphere2.SetActive (false);
-		sphere3.SetActive (false);
-		sphere4.SetActive (false);
-		sphere5.SetActive (false);
-		sphere6.SetActive (false);
-		sphere7.SetActive (true);
+	void DotPosition_7(){
+		Dot1.SetActive (false);
+		Dot2.SetActive (false);
+		Dot3.SetActive (false);
+		Dot4.SetActive (false);
+		Dot5.SetActive (false);
+		Dot6.SetActive (false);
+		Dot7.SetActive (true);
 	}
 }
