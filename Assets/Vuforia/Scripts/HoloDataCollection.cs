3947452b1b8554294b7efdc08306aaf24549168f
CTCbaxia <ctcbaxia@gmail.com>
2017-08-08 12:27:06 +0100

    revise bugs

--- Assets/Vuforia/Scripts/HoloDataCollection.cs
+++ Assets/Vuforia/Scripts/HoloDataCollection.cs
@@ -82,7 +82,7 @@ public class HoloDataCollection : MonoBehaviour
 		async () => {
 
 		StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(folderName);
-		StorageFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExisted);//create file
+		StorageFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);//create file
 
 		await FileIO.AppendTextAsync(file, "Condition,Task,Offset,Answer"+"\r\n");
 		await FileIO.AppendTextAsync(file, "1,1,1,1"+"\r\n");
